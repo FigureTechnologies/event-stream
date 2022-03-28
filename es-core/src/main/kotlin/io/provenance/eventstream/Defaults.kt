29fe737cb12912c169d4cd9cd300ba01fd95a2b5
Phil Story <mtps@users.noreply.github.com>
2022-03-28 10:07:11 -0600

    Add new defaults to expose components for customization

--- es-core/src/main/kotlin/io/provenance/eventstream/Defaults.kt
+++ es-core/src/main/kotlin/io/provenance/eventstream/Defaults.kt
@@ -7,20 +7,24 @@ import com.sksamuel.hoplite.preprocessor.PropsPreprocessor
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.lifecycle.LifecycleRegistry
 import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
 import com.tinder.scarlet.websocket.okhttp.newWebSocketFactory
 import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
 import io.provenance.blockchain.stream.api.BlockSource
 import io.provenance.eventstream.adapter.json.JSONObjectAdapter
+import io.provenance.eventstream.adapter.json.decoder.DecoderEngine
 import io.provenance.eventstream.adapter.json.decoder.MoshiDecoderEngine
 import io.provenance.eventstream.config.Config
 import io.provenance.eventstream.config.Environment
 import io.provenance.eventstream.stream.BlockStreamOptions
 import io.provenance.eventstream.stream.DefaultBlockStreamFactory
+import io.provenance.eventstream.stream.EventStreamService
+import io.provenance.eventstream.stream.TendermintEventStreamService
+import io.provenance.eventstream.stream.TendermintRPCStream
 import io.provenance.eventstream.stream.TendermintServiceClient
 import io.provenance.eventstream.stream.clients.TendermintBlockFetcher
 import io.provenance.eventstream.stream.clients.TendermintServiceOpenApiClient
-import io.provenance.eventstream.stream.infrastructure.Serializer.moshi
 import io.provenance.eventstream.stream.models.StreamBlockImpl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import okhttp3.OkHttpClient
@@ -38,7 +42,6 @@ fun defaultOkHttpClient(pingInterval: Duration = Duration.seconds(10), readInter
         .pingInterval(pingInterval.inWholeMilliseconds, TimeUnit.MILLISECONDS)
         .readTimeout(readInterval.inWholeMilliseconds, TimeUnit.MILLISECONDS)
         .build()
-
 /**
  * Create a default configuration to use for the event stream.
  *
@@ -82,7 +85,15 @@ fun defaultMoshi(): Moshi = Moshi.Builder()
     .add(JSONObjectAdapter())
     .build()
 
-fun decoderEngine() = MoshiDecoderEngine(moshi)
+/**
+ * Create the default [Moshi] [DecoderEngine]
+ *
+ * @param moshi The moshi instance to use in the decoder engine.
+ * @return The [DecoderEngine] instance to use for the event stream.
+ */
+fun defaultDecoderEngine(moshi: Moshi = defaultMoshi()): DecoderEngine =
+    MoshiDecoderEngine(moshi)
+
 /**
  * Create the default [TendermintServiceClient] to use with the event stream.
  *
@@ -104,11 +115,22 @@ fun defaultTendermintFetcher(rpcUri: String): TendermintBlockFetcher =
 /**
  *
  */
+fun defaultEventStreamService(uri: String, okHttpClient: OkHttpClient, throttle: Duration): EventStreamService {
+    val eventStreamBuilder = defaultEventStreamBuilder(uri, okHttpClient)
+    val lifecycle = LifecycleRegistry(throttle.inWholeMilliseconds)
+    val scarlet: Scarlet = eventStreamBuilder.lifecycle(lifecycle).build()
+    val tendermintRpc: TendermintRPCStream = scarlet.create(TendermintRPCStream::class.java)
+    return TendermintEventStreamService(tendermintRpc, lifecycle)
+}
+
+/**
+ *
+ */
 @OptIn(ExperimentalCoroutinesApi::class, ExperimentalTime::class)
 fun defaultEventStream(config: Config, options: BlockStreamOptions, okHttpClient: OkHttpClient = defaultOkHttpClient(), moshi: Moshi = defaultMoshi(), fetcher: TendermintBlockFetcher = defaultTendermintFetcher(config.eventStream.rpc.uri)): BlockSource<StreamBlockImpl> {
     val factory = DefaultBlockStreamFactory(
         config = config,
-        decoderEngine = decoderEngine(),
+        decoderEngine = defaultDecoderEngine(moshi),
         eventStreamBuilder = defaultEventStreamBuilder(config.eventStream.websocket.uri, okHttpClient),
         blockFetcher = fetcher
     )
