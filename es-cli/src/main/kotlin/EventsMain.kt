f177bf80c44338bd6194a13a02840c52d2057bb6
Phil Story <mtps@users.noreply.github.com>
2022-03-29 22:30:24 -0600

    Add toLiveStream in EventMain to show usage related to LiveMetaDataStream

--- es-cli/src/main/kotlin/EventsMain.kt
+++ es-cli/src/main/kotlin/EventsMain.kt
@@ -6,6 +6,7 @@ import io.provenance.eventstream.net.defaultOkHttpClient
 import io.provenance.eventstream.net.okHttpNetAdapter
 import io.provenance.eventstream.stream.nodeEventStream
 import io.provenance.eventstream.stream.rpc.response.MessageType
+import io.provenance.eventstream.stream.toLiveMetaDataStream
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 
@@ -14,8 +15,9 @@ fun main() = runBlocking {
     val netAdapter = okHttpNetAdapter("ws://rpc.test.provenance.io:26657", okHttp)
     val decoderAdapter = moshiDecoderAdapter()
 
-    nodeEventStream<MessageType.NewBlockHeader>(netAdapter, decoderAdapter)
-        .collect { println("newBlockHeader:\n$it") }
+    nodeEventStream<MessageType.NewBlock>(netAdapter, decoderAdapter)
+        .toLiveMetaDataStream()
+        .collect { println("newBlock:\n$it") }
 
     okHttp.awaitShutdown()
 }
