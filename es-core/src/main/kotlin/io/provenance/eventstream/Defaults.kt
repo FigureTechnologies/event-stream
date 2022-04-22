43ccd5b0ba3a2af984403ea6b8036cf81d49d1b8
Phil Story <mtps@users.noreply.github.com>
2022-04-22 10:08:37 -0600

    Add backoff strategy and remove unused factory methods

--- es-core/src/main/kotlin/io/provenance/eventstream/Defaults.kt
+++ es-core/src/main/kotlin/io/provenance/eventstream/Defaults.kt
@@ -1,46 +1,18 @@
 package io.provenance.eventstream
 
-import com.sksamuel.hoplite.ConfigLoader
-import com.sksamuel.hoplite.EnvironmentVariablesPropertySource
-import com.sksamuel.hoplite.PropertySource
-import com.sksamuel.hoplite.preprocessor.PropsPreprocessor
 import com.tinder.scarlet.MessageAdapter
 import com.tinder.scarlet.Scarlet
 import com.tinder.scarlet.WebSocket
 import com.tinder.scarlet.lifecycle.LifecycleRegistry
-import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.retry.BackoffStrategy
+import com.tinder.scarlet.retry.ExponentialBackoffStrategy
 import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
-import io.provenance.blockchain.stream.api.BlockSource
-import io.provenance.eventstream.adapter.json.decoder.DecoderEngine
-import io.provenance.eventstream.adapter.json.decoder.MoshiDecoderEngine
-import io.provenance.eventstream.config.Config
-import io.provenance.eventstream.config.Environment
-import io.provenance.eventstream.decoder.defaultMoshi
-import io.provenance.eventstream.net.NetAdapter
-import io.provenance.eventstream.net.defaultOkHttpClient
-import io.provenance.eventstream.net.okHttpNetAdapter
-import io.provenance.eventstream.stream.BlockStreamOptions
-import io.provenance.eventstream.stream.DefaultBlockStreamFactory
-import io.provenance.eventstream.stream.TendermintServiceClient
 import io.provenance.eventstream.stream.WebSocketChannel
-import io.provenance.eventstream.stream.clients.TendermintBlockFetcher
-import io.provenance.eventstream.stream.clients.TendermintServiceOpenApiClient
 import io.provenance.eventstream.stream.flows.DEFAULT_THROTTLE_PERIOD
-import io.provenance.eventstream.stream.models.StreamBlockImpl
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import okhttp3.OkHttpClient
+import kotlinx.coroutines.CancellationException
+import mu.KotlinLogging
 import java.lang.reflect.Type
 import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
-
-/**
- * Create the default [TendermintServiceClient] to use with the event stream.
- *
- * @param rpcUri The URI of the Tendermint RPC service to connect to.
- * @return The [TendermintServiceClient] instance to use for the event stream.
- */
-fun defaultTendermintService(rpcUri: String): TendermintServiceClient =
-    TendermintServiceOpenApiClient(rpcUri)
 
 /**
  * Adapter type wrapper for [WebSocket.Factory] lambdas.
@@ -71,6 +43,30 @@ fun WsDecoderAdapter.toScarletFactory(): MessageAdapter.Factory {
 }
 
 /**
+ * Create a sane [BackoffStrategy] that will bail after a maximum number of retries.
+ *
+ * @param maxRetries The maximum number of retries to attempt
+ * @param fallbackStrategy The fallback [BackoffStrategy] to reference if maxRetries is not exceeded.
+ */
+fun defaultBackoffStrategy(
+    maxRetries: Int = 10,
+    fallbackStrategy: BackoffStrategy = ExponentialBackoffStrategy(1000L, 10000L)
+): BackoffStrategy {
+    return object : BackoffStrategy {
+        val log = KotlinLogging.logger {}
+        override fun backoffDurationMillisAt(retryCount: Int): Long {
+            if (retryCount > maxRetries) {
+                throw CancellationException("max retry count of $maxRetries exceeded")
+            }
+
+            return fallbackStrategy.backoffDurationMillisAt(retryCount).also {
+                log.info { "backing off ${it}ms before next connection attempt" }
+            }
+        }
+    }
+}
+
+/**
  * Create a [WebSocketChannel] instance via [Scarlet].
  *
  * @param wsFactory The [WsAdapter] used to create [WebSocket] used within [Scarlet] to connect to a host.
@@ -84,8 +80,10 @@ fun defaultWebSocketChannel(
     msgAdapterFactory: WsDecoderAdapter,
     throttle: Duration = DEFAULT_THROTTLE_PERIOD,
     lifecycle: LifecycleRegistry = defaultLifecycle(throttle),
+    backoffStrategy: BackoffStrategy = defaultBackoffStrategy(),
 ): WebSocketChannel {
     val scarlet = Scarlet.Builder()
+        .backoffStrategy(backoffStrategy)
         .webSocketFactory(wsFactory.toScarletFactory())
         .addMessageAdapterFactory(msgAdapterFactory.toScarletFactory())
         .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
@@ -99,71 +97,3 @@ fun defaultWebSocketChannel(
  */
 fun defaultLifecycle(throttle: Duration = DEFAULT_THROTTLE_PERIOD): LifecycleRegistry =
     LifecycleRegistry(throttle.inWholeMilliseconds)
-
-/**
- * ---------------------------------------------------------------------------
- * Old stuff - TODO work to remove this
- * ---------------------------------------------------------------------------
- */
-
-/**
- *
- */
-@OptIn(ExperimentalCoroutinesApi::class, ExperimentalTime::class)
-fun defaultEventStream(
-    config: Config,
-    options: BlockStreamOptions,
-    okHttpClient: OkHttpClient = defaultOkHttpClient(),
-    decoderEngine: DecoderEngine = MoshiDecoderEngine(defaultMoshi()),
-    fetcher: TendermintBlockFetcher = defaultTendermintFetcher(config.eventStream.rpc.uri)
-): BlockSource<StreamBlockImpl> {
-    val factory = DefaultBlockStreamFactory(
-        config = config,
-        decoderEngine = decoderEngine,
-        eventStreamBuilder = defaultEventStreamBuilder(okHttpNetAdapter(config.eventStream.websocket.uri, okHttpClient)),
-        blockFetcher = fetcher
-    )
-    return factory.createSource(options)
-}
-
-/**
- * Create the default [TendermintServiceClient] to use with the event stream.
- *
- * @param rpcUri The URI of the Tendermint RPC service to connect to.
- * @return The [TendermintServiceClient] instance to use for the event stream.
- */
-fun defaultTendermintFetcher(rpcUri: String): TendermintBlockFetcher =
-    TendermintBlockFetcher(TendermintServiceOpenApiClient(rpcUri))
-
-/**
- * Create a default websocket builder for the event stream.
- */
-@OptIn(ExperimentalTime::class)
-fun defaultEventStreamBuilder(netAdapter: NetAdapter): Scarlet.Builder {
-    val factory = object : WebSocket.Factory {
-        override fun create(): WebSocket = netAdapter.wsAdapter.invoke()
-    }
-    return Scarlet.Builder()
-        .webSocketFactory(factory)
-        .addMessageAdapterFactory(MoshiMessageAdapter.Factory())
-        .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
-}
-
-/**
- * Create a default configuration to use for the event stream.
- *
- * @param environment The environment to generate a configutation with.
- * @return The configuration
- */
-fun defaultConfig(environment: Environment): Config = ConfigLoader.Builder()
-    .addSource(EnvironmentVariablesPropertySource(useUnderscoresAsSeparator = true, allowUppercaseNames = true))
-    .apply {
-        // If in the local environment, override the ${...} envvar values in `application.properties` with
-        // the values provided in the local-specific `local.env.properties` property file:
-        if (environment.isLocal()) {
-            addPreprocessor(PropsPreprocessor("/local.env.properties"))
-        }
-    }
-    .addSource(PropertySource.resource("/application.yml"))
-    .build()
-    .loadConfigOrThrow()
