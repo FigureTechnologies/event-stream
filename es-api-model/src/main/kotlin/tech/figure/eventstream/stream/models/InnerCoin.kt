e52df662f6d837addeb017e15e620616aaa924f1
Jake Schwartz <jschwartz@figure.com>
2023-02-03 09:35:34 -0800

    InnerCoin amount from String -> BigInteger

--- es-api-model/src/main/kotlin/tech/figure/eventstream/stream/models/InnerCoin.kt
+++ es-api-model/src/main/kotlin/tech/figure/eventstream/stream/models/InnerCoin.kt
@@ -1,7 +1,8 @@
 package tech.figure.eventstream.stream.models
 
 import cosmos.base.v1beta1.CoinOuterClass.Coin
+import java.math.BigInteger
 
-data class InnerCoin(val amount: String, val denom: String) {
-    constructor(coin: Coin) : this(amount = coin.amount, denom = coin.denom)
+data class InnerCoin(val amount: BigInteger, val denom: String) {
+    constructor(coin: Coin) : this(amount = coin.amount.toBigIntegerOrNull() ?: BigInteger.ZERO, denom = coin.denom)
 }
