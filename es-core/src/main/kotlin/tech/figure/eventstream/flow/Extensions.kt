e2e2195c0ff693aa8003da4ec1874f4edefd78df
Phil Story <mtps@users.noreply.github.com>
2023-04-27 17:22:30 -0600

    Grammar, unused annotations, and linter fixes

--- es-core/src/main/kotlin/tech/figure/eventstream/flow/Extensions.kt
+++ es-core/src/main/kotlin/tech/figure/eventstream/flow/Extensions.kt
@@ -1,9 +1,7 @@
 package tech.figure.eventstream.flow
 
-import tech.figure.eventstream.stream.models.BlockMeta
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
@@ -18,6 +16,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import tech.figure.eventstream.stream.models.BlockMeta
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.time.Duration
@@ -35,7 +34,7 @@ val DEFAULT_POLL_INTERVAL = 2.seconds
  *
  * @param signal When an item is received on the channel, the underlying flow will be cancelled.
  */
-@OptIn(ExperimentalStdlibApi::class, InternalCoroutinesApi::class, FlowPreview::class)
+@OptIn(InternalCoroutinesApi::class)
 fun <T> Flow<T>.cancelOnSignal(signal: Channel<Unit>): Flow<T> = flow {
     val outer = this
     try {
@@ -68,7 +67,6 @@ fun <T> Flow<T>.cancelOnSignal(signal: Channel<Unit>): Flow<T> = flow {
  * @param timeout If an element is not emitted in the specified duration, the buffer will be emitted downstream if
  *  it is non-empty.
  */
-@OptIn(FlowPreview::class, ExperimentalTime::class)
 fun <T> Flow<T>.chunked(size: Int, timeout: Duration? = null): Flow<Flow<T>> =
     chunked(size = size, timeout = timeout) { it }
 
@@ -76,24 +74,20 @@ private suspend fun <T> getChunk(channel: Channel<T>, maxChunkSize: Int): List<T
     val received = channel.receive()
     val chunk = mutableListOf(received)
     while (chunk.size < maxChunkSize) {
-        val polled = channel.poll()
-        if (polled == null) {
-            return chunk
-        }
+        val polled = channel.tryReceive().getOrNull() ?: return chunk
         chunk.add(polled)
     }
     return chunk
 }
 
 /**
- * Returns a flow of lists each not exceeding the given [size].
- * The last list in the resulting flow may have less elements than the given [size].
+ * Returns a flow of lists each not exceeding the given [maxSize].
+ * The last list in the resulting flow may have fewer elements than the given [maxSize].
  *
- * @param size the number of elements to take in each list, must be positive and can be greater than the number of elements in this flow.
- * @param timeout If an element is not emitted in the specified duration, the buffer will be emitted downstream if
- *  it is non-empty.
+ * @param maxSize the number of elements to take in each list, must be positive and can be greater than the number of elements in this flow.
+ * @param endHeight
  */
-@OptIn(InternalCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 fun <T> Flow<T>.chunked(maxSize: Int, endHeight: Long): Flow<List<T>> {
     val buffer = Channel<T>(maxSize)
     return channelFlow {
@@ -128,7 +122,7 @@ fun <T> Flow<T>.chunked(maxSize: Int, endHeight: Long): Flow<List<T>> {
  *
  * Note that the list passed to the [transform] function is ephemeral and is valid only inside that function.
  * You should not store it or allow it to escape in some way, unless you made a snapshot of it.
- * The last list may have less elements than the given [size].
+ * The last list may have fewer elements than the given [size].
  *
  * This is more efficient, than using flow.chunked(n).map { ... }
  *
@@ -136,7 +130,6 @@ fun <T> Flow<T>.chunked(maxSize: Int, endHeight: Long): Flow<List<T>> {
  * @param timeout If an element is not emitted in the specified duration, the buffer will be emitted downstream if
  *  it is non-empty.
  */
-@OptIn(FlowPreview::class, ExperimentalTime::class)
 fun <T, R> Flow<T>.chunked(size: Int, timeout: Duration? = null, transform: suspend (Flow<T>) -> R): Flow<R> {
     require(size > 0) { "Size should be greater than 0, but was $size" }
     return windowed(size = size, step = size, partialWindows = true, timeout = timeout, transform = transform)
@@ -147,27 +140,26 @@ fun <T, R> Flow<T>.chunked(size: Int, timeout: Duration? = null, transform: susp
  * sliding along this flow with the given [step], where each
  * snapshot is a list.
  *
- * Several last lists may have less elements than the given [size].
+ * Several last lists may have fewer elements than the given [size].
  *
  * Both [size] and [step] must be positive and can be greater than the number of elements in this flow.
  * @param size the number of elements to take in each window
  * @param step the number of elements to move the window forward by on an each step
- * @param partialWindows controls whether or not to keep partial windows in the end if any.
+ * @param partialWindows controls whether to keep partial windows in the end if any.
  * @param timeout If an element is not emitted in the specified duration, the buffer will be emitted downstream if
  *  it is non-empty.
  */
-@OptIn(FlowPreview::class, ExperimentalTime::class)
 fun <T> Flow<T>.windowed(size: Int, step: Int, partialWindows: Boolean, timeout: Duration? = null): Flow<Flow<T>> =
     windowed(size = size, step = step, partialWindows = partialWindows, timeout = timeout) { it }
 
 /**
  * Returns a flow of results of applying the given [transform] function to
- * an each list representing a view over the window of the given [size]
+ * a list representing a view over the window of the given [size]
  * sliding along this collection with the given [step].
  *
  * Note that the list passed to the [transform] function is ephemeral and is valid only inside that function.
  * You should not store it or allow it to escape in some way, unless you made a snapshot of it.
- * Several last lists may have less elements than the given [size].
+ * Several last lists may have fewer elements than the given [size].
  *
  * This is more efficient, than using flow.windowed(...).map { ... }
  *
@@ -183,8 +175,6 @@ fun <T> Flow<T>.windowed(size: Int, step: Int, partialWindows: Boolean, timeout:
     InternalCoroutinesApi::class,
     ExperimentalCoroutinesApi::class,
     ExperimentalTime::class,
-    ExperimentalStdlibApi::class,
-    FlowPreview::class,
 )
 fun <T, R> Flow<T>.windowed(
     size: Int,
